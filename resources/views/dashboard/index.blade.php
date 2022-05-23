c47051a2f73c2a2cc69e1ab8003dc0fa230f5c3b
edpanxd <aguirreini.xd@gmail.com>
2022-05-23 08:42:37 -0500

    Actualizacion V 0.4

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -434,7 +434,7 @@
                     <span class="info-box-icon "><i class="fas fa-wallet"></i></span>
                     <div class="info-box-content">
                         <h5 class="info-box-text"> </h5>
-                        <h2 class="info-box-number">{{ $sumaA }}</h2>
+                        <h2 class="info-box-number">{{$formato= number_format($sumaA,2)  }}</h2>
                     </div>
                     <!-- /.info-box-content -->
                 </div>
